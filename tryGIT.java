a64d5fa74c3f17fd263b5e27e8fbb9f73d4351cd
annisyaap <50549201+annisyaap@users.noreply.github.com>
2019-11-05 10:42:48 +0700

    vers 3
    Versi 3

--- tryGIT.java
+++ tryGIT.java
@@ -10,7 +10,10 @@ public class tryGIT {
 		// TODO Auto-generated method stub
 		System.out.println("Latihan GIT doang");
 		System.out.println("Bisa dong yaaaaa");
-
+		int a = 1;
+		int b = 3;
+		int c = a+b;
+		System.out.println("Hasil C = "+c);
 
 	}
 
