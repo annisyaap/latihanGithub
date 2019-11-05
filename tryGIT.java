f484cd165e156d177ad33e665ae08dbcf0670e5e
annisyaap <50549201+annisyaap@users.noreply.github.com>
2019-11-05 09:59:35 +0700

    versi 1
    trying GIT ver 1

---
+++ tryGIT.java
@@ -0,0 +1,16 @@
+package learningGIT;
+
+public class tryGIT {
+
+	public tryGIT() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Latihan GIT doang");
+			System.out.println("Aku anggota kelompok 6");
+
+	}
+
+}
